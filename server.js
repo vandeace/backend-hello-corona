3cc2b9d7ed4d752b150b92687d7a0f76bcfd2582
Muhammad Jafar Ash Shiddiq <jafar@MacBook-Pro.local>
2020-05-04 15:57:35 +0700

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = Number(process.env.PORT || 6000);
 const route = require('./routes/index');
 const cors = require('cors');
 app.use(cors());
